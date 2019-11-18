3dc2e4f41c79eb2c26bdab57403eca5016bc73ae
Jarrod Kallis <jarrod.kallis@altron.com>
2019-11-18 17:50:03 +0200

    Generate Java Objects from XSD

--- src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/SoapCourseManagementApplication.java
+++ src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/SoapCourseManagementApplication.java
@@ -8,6 +8,8 @@ public class SoapCourseManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoapCourseManagementApplication.class, args);
+
+		System.out.println(args);
 	}
 
 }
